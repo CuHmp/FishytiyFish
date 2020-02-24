d5db945a612f85956159fa5992ce18374aa6471d
Adam Olsson <adaols80@gmail.com>
2020-02-24 14:08:03 +0100

    Cleaning and tweaking

--- FishytyFishFish/Assets/Scripts/DialogMaster.cs
+++ FishytyFishFish/Assets/Scripts/DialogMaster.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+
 public class DialogMaster : MonoBehaviour {
 
     public static List<question> question_list = new List<question>();
@@ -9,31 +9,27 @@ public class DialogMaster : MonoBehaviour {
     public float text_delay = 0.125f;
 
     private bool hasCreatedButtons = false;
-    // Start is called before the first frame update
+
     void Start() {
         AddButton.buttonPrefab = buttonPrefab;
 
-        question q1 = new question("Hello Fellow human would you like a coffee?", text_delay);
-        question q2 = new question("Aha I understand your situation now please be ready to die", text_delay / 2);
-
-
-        List<anwser> list = new List<anwser>();
-        list.Add(new anwser("test", q2));
-        list.Add(new anwser("qwerts", q2));
-        list.Add(new anwser("Yeet The Feet", q2));
-        q1.anwser = list;
-        question_list.Add(q1);
-        List<anwser> lists = new List<anwser>();
-        lists.Add(new anwser("test1", null));
-        lists.Add(new anwser("asdf", null));
-        lists.Add(new anwser("Leet Mega Haxor", null));
-        q2.anwser = lists;
-        question_list.Add(q2);
+        question_list.Add(QuestionFactory.CreateQuestion("Aha I understand your situation now please be ready to die",
+                                                        text_delay, new string[] { "test1", "asdf", "Leet Mega Haxor" },
+                                                        new question[] { null,null,null }, 3));
+        question_list.Add(QuestionFactory.CreateQuestion("Here you go you litle shit",
+                                                         text_delay, new string[] { ";(", "What", "Stab officer" },
+                                                         new question[] { question_list[0], question_list[0], question_list[0] }, 3));
+        question_list.Add(QuestionFactory.CreateQuestion("The Cop leave you alone in the room to grab a coffy",
+                                                 text_delay, new string[] { "Okey" },
+                                                 new question[] { question_list[1] }, 1));
+        question_list.Add(QuestionFactory.CreateQuestion("Hello Fellow human would you like a coffee?",
+                                                 text_delay, new string[] { "Yes", "No", "Draw gun" },
+                                                 new question[] { question_list[2], question_list[1], question_list[0] }, 3));
+        question_list.Reverse();
 
         question_cursor = question_list[0];
     }
 
-    // Update is called once per frame
     void Update() {
         // start the printing of the text
         if (!TypeWriter.is_done && !TypeWriter.is_writing) {
@@ -48,20 +44,26 @@ public class DialogMaster : MonoBehaviour {
             hasCreatedButtons = true;
         }
     }
+
     public void OnClick(int index) {
         // Remove all the previous buttons
         Transform parent = GameObject.Find("CommandPanel").transform;
         foreach (Transform child in parent) {
             Destroy(child.gameObject);
         }
+        hasCreatedButtons = false;
 
         // Go to the corresponding button press question
         if (question_cursor.anwser[index].next_question != null) {
             question_cursor = question_cursor.anwser[index].next_question;
         }
+        else {
+            hasCreatedButtons = true;
+            TypeWriter.StartTypewriter("Game over, thank you for playing");
+        }
         // reset the typewriter and button creator
         TypeWriter.is_done = false;
-        hasCreatedButtons = false;
+
     }
 }
 
